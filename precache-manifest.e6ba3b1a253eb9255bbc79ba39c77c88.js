e3db7f7aedb66f2a465c76f149b756a8a13a433f
Nikodem Graczewski <nikodem.graczewski@in4mo.com>
2019-12-10 16:00:47 +0100

    Init

---
+++ precache-manifest.e6ba3b1a253eb9255bbc79ba39c77c88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a951da93e41e268a5a4fafb6143a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29f2f500fedac338a6d8",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "7ba817dd049d3aca3615",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "29f2f500fedac338a6d8",
+    "url": "/static/js/2.63d1be80.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.63d1be80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ba817dd049d3aca3615",
+    "url": "/static/js/main.d1c5e372.chunk.js"
+  },
+  {
+    "revision": "b6f8cb8cf985e34bf045",
+    "url": "/static/js/runtime-main.51c4ffa8.js"
+  }
+]);
